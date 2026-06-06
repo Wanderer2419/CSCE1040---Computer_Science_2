f4d6a7eec25de7f9100723faff6d9c859dab005f
Wanderer2419 <wargamer34567@gmail.com>
2026-06-05 19:54:07 -0500

    Update student.h

--- module1/HW1/student.h
+++ module1/HW1/student.h
@@ -1,1 +1,17 @@
+#ifndef STUDENT_H
+#define STUDENT_H
 
+#include <string>
+using namespace std;
+
+struct Student {
+	int idNum;
+	string firstName;
+	string lastName;
+	int exam1;
+	int exam2;
+	int exam3;
+	float mean;
+}
+
+#endif
