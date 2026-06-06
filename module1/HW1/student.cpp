3874c2280c3e9820e44c16595c2013906518fb44
Wanderer2419 <wargamer34567@gmail.com>
2026-06-05 22:42:01 -0500

    Update student.cpp

--- module1/HW1/student.cpp
+++ module1/HW1/student.cpp
@@ -1,1 +1,26 @@
+#include <iostream>
+#include <string>
+using namespace std;
+#include "student.h"
 
+void displayStudents(Student students[], int numStudents) {
+	cout << "*********************** Students Information ***********************" << endl;
+	cout << setw(10) << left << "Id"
+		<< setw(15) << left << "First name"
+		<< setw(15) << left << "Last name"
+		<< setw(5) << right << "Exam1"
+		<< setw(5) << right << "Exam2"
+		<< setw(5) << right << "Exam3"
+		<< setw(5) << right << "Mean" << endl;
+	cout << "_____________________________________________________________________" << endl;
+	for (int i = 0; i < numStudents; i++) {
+		cout << fixed << setprecision(2)
+			<< setw(10) << left << students[i].idNum
+			<< setw(15) << left << students[i].firstName
+			<< setw(15) << left << students[i].lastName
+			<< setw(5) << right << students[i].exam1
+			<< setw(5) << right << students[i].exam2
+			<< setw(5) << right << students[i].exam3
+			<< setw(5) << right << students[i].mean << endl;
+	}
+}
