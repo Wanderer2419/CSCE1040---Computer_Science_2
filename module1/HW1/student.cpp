92cf6d747e2a25068536412db21f9d7521c2d8dc
Wanderer2419 <wargamer34567@gmail.com>
2026-06-05 23:35:46 -0500

    Update student.cpp

--- module1/HW1/student.cpp
+++ module1/HW1/student.cpp
@@ -9,19 +9,19 @@ void displayStudents(Student students[], int numStudents) {
 	cout << setw(10) << left << "Id"
 		<< setw(15) << left << "First name"
 		<< setw(15) << left << "Last name"
-		<< setw(5) << right << "Exam1"
-		<< setw(5) << right << "Exam2"
-		<< setw(5) << right << "Exam3"
-		<< setw(5) << right << "Mean" << endl;
+		<< setw(8) << right << "Exam1"
+		<< setw(8) << right << "Exam2"
+		<< setw(8) << right << "Exam3"
+		<< setw(8) << right << "Mean" << endl;
 	cout << "_____________________________________________________________________" << endl;
 	for (int i = 0; i < numStudents; i++) {
 		cout << fixed << setprecision(2)
 			<< setw(10) << left << students[i].idNum
 			<< setw(15) << left << students[i].firstName
 			<< setw(15) << left << students[i].lastName
-			<< setw(5) << right << students[i].exam1
-			<< setw(5) << right << students[i].exam2
-			<< setw(5) << right << students[i].exam3
-			<< setw(5) << right << students[i].mean << endl;
+			<< setw(8) << right << students[i].exam1
+			<< setw(8) << right << students[i].exam2
+			<< setw(8) << right << students[i].exam3
+			<< setw(8) << right << students[i].mean << endl;
 	}
 }
