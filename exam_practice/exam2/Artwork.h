1fc54c0b96fdd99c1ab340e0f3116bb0456e382e
Wanderer2419 <wargamer34567@gmail.com>
2026-06-22 10:25:58 -0500

    Update Artwork.h

--- exam_practice/exam2/Artwork.h
+++ exam_practice/exam2/Artwork.h
@@ -1,1 +1,29 @@
+#ifndef ARTWORK_H
+#define ARTWORK_H
 
+#include <string>
+using namespace std;
+
+class Artwork {
+	private:
+		string Title;
+		string ArtistName;
+		int* id;
+	public:
+		// constructors, desctructors, copy assignment, and print
+		Artwork();		// default constructor
+		~Artwork();		// destructor
+		Artwork(const Artwork& origObj);	// copy constructor
+		Artwork& operator=(const Artwork& rhs);	// copy assignment
+		void Print();	// print function
+		// mutators
+		void setTitle(string title);
+		void setName(string name);
+		void setId(int id);
+		// accessors
+		string getTitle();
+		string getName();
+		int getId();
+}
+
+#endif ARTWORK_H
