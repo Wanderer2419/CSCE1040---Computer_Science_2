c1e6bc8a2dfe0e36f36010e06ede18ebf035741f
Wanderer2419 <wargamer34567@gmail.com>
2026-06-24 11:09:59 -0500

    Create stackmain.cpp

---
+++ module5/stackmain.cpp
@@ -0,0 +1,21 @@
+#include "stack.h"
+#include <iostream>
+using namespace std;
+
+int main() {
+	Stack s;
+
+	//push
+	s.push(10);
+	s.push(40);
+	s.push(20);
+	s.push(26);
+	s.print();
+
+	//pop
+	s.pop;
+	s.pop;
+	s.print();
+
+	return 0;
+}
