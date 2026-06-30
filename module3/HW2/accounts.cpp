b6cccc9da785fa1da1a0b4c4d97aeb0467ff1838
Wanderer2419 <wargamer34567@gmail.com>
2026-06-30 15:19:42 -0500

    Update accounts.cpp

--- module3/HW2/accounts.cpp
+++ module3/HW2/accounts.cpp
@@ -1,1 +1,33 @@
+/*
+		▪ Default constructor
+			• Allocate memory for a dynamic array of size defined by the maximum
+				size (private data member) and initialize size to zero.
+		▪ Destructor
+			• Deallocate memory that was allocated in the constructors and reset
+				the size (counter).
+		▪ Add account to the array – takes an object of the Account class as an
+			argument (pass by reference).
+		▪ Find an object of the Account class in the dynamic array based on the
+			account ID – takes the account ID as an argument.
+		▪ at method that returns a refence of an element (i.e., an object of the
+			Account class) in the dynamic array at the given index.
+*/
 
+#include "accounts.h"
+using namespace std;
+//const int SIZE = 1000;
+//Account* first;
+//int countSize;
+
+		// constructors
+		Accounts() {	//default constructor
+			
+		}
+		~Accounts() {	// destructor
+			
+		}
+
+		// methods
+		void addAccount(const Acount& input);
+		Account searchID(const int id);
+		Account& at(const int i) const;
