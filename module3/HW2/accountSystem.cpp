0f72f89e57aadfb29995a335e02af8074f8f67fb
Wanderer2419 <wargamer34567@gmail.com>
2026-06-30 16:08:17 -0500

    Update accountSystem.cpp

--- module3/HW2/accountSystem.cpp
+++ module3/HW2/accountSystem.cpp
@@ -1,1 +1,45 @@
+Accounts accts;
+#include "accountSystem.h"
+#include <iostream>
+#include <iomanip>
+#include <string>
+using namespace std;
 
+void AccountSystem::AddAccount() const {
+	Account input;
+	int id;
+	string name;
+	float deposit;
+
+	// read from user
+	cout << "Enter account id: ";
+	cin >> id;
+	if (id == accts.SearchID(id).GetID()) {
+		cout << "Error: Account with ID " << id << " already exists." << endl;
+	} else {
+		cout << "Enter customer account name: ";
+		getline(cin, name);
+		cin.ignore();
+		cout << "Enter initial deposit amount: $";
+		cin >> deposit;
+		// use mutators
+		input.SetID(id);
+		input.SetCustName(name);
+		input.SetAcctBalance(deposit);
+		// pass input into the accounts
+		accts.addAccount(input);
+	}
+}
+
+void AccountSystem::Deposit() const {
+	
+}
+void AccountSystem::Withdraw() const {
+	
+}
+void AccountSystem::Transfer() const {
+	
+}
+void AccountSystem::PrintAll() const {
+	
+}
