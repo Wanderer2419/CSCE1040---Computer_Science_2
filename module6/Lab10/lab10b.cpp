a5c079feba9d69a478b9390d6c1f6bef6d521b81
Wanderer2419 <wargamer34567@gmail.com>
2026-07-03 22:58:18 -0500

    Update lab10b.cpp

--- module6/Lab10/lab10b.cpp
+++ module6/Lab10/lab10b.cpp
@@ -1,1 +1,21 @@
+#include <iostream>
+using namespace std;
 
+/* TODO: Write recursive DigitCount() function here. */
+int DigitCount(int num) {
+	int numDigits = 1;
+	if ((num / 10) > 0) {
+		numDigits += DigitCount(num/10);
+	}
+	return numDigits;
+}
+
+int main() {
+	int num;
+	int digits;
+	
+	cin >> num;
+	digits = DigitCount(num);
+	cout << digits << endl;
+	return 0;
+}
