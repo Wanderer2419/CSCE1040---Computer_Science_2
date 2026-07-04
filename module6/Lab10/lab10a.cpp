6d3542b900a55f03213319f451952db587d2be97
Wanderer2419 <wargamer34567@gmail.com>
2026-07-03 22:40:12 -0500

    Update lab10a.cpp

--- module6/Lab10/lab10a.cpp
+++ module6/Lab10/lab10a.cpp
@@ -1,1 +1,22 @@
+#include <iostream>
+using namespace std;
 
+// TODO: Write recursive PrintNumPattern() function
+void PrintNumPattern(int num1, int num2) {
+	if (num1 >= 0) {
+		cout << num1 << " ";
+		PrintNumPattern(num1 - num2, num2);
+	}
+	cout << num1 << " ";
+}
+
+int main() {
+	int num1;
+	int num2;
+	
+	cin >> num1;
+	cin >> num2;
+	PrintNumPattern(num1, num2);
+	
+	return 0;
+}
