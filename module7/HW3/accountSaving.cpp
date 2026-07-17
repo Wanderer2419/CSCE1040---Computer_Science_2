6a92564b85200fe5a7fd605eae4d6bd9955b3165
Wanderer2419 <wargamer34567@gmail.com>
2026-07-17 00:41:09 -0500

    Create accountSaving.cpp

---
+++ module7/HW3/accountSaving.cpp
@@ -0,0 +1,56 @@
+#include "accountSaving.h"
+#include <iostream>
+#include <iomanip>
+#include <string>
+using namespace std;
+
+public:
+// constructors
+AccountSaving() { // default constructor
+	minimumBalance = 0.00;
+}
+~AccountSaving() { // destructor
+	delete ID;
+}
+AccountSaving (const AccountSaving& origObj) { // copy constructor
+	ID = new int;
+	*ID = origObj.GetID();
+	customerName = origObj.GetCustName();
+	acctBalance = origObj.GetAcctBalance();
+	transactionFee = origObj.GetMinBalance();
+}
+// operators
+AccountSaving& operator=(const AccountSaving& rhs) {
+	if (this != &rhs) {
+		delete ID;
+		ID = new int;
+		*ID = rhs.GetID();
+		customerName = rhs.GetCustName();
+		acctBalance = rhs.GetAcctBalance();
+		minimumBalance = origObj.GetMinBalance();
+	}
+	return *this;
+}
+// minimumBalance functions
+void SetMinBalance(float min) {
+	minimumBalance = min;
+}
+float GetMinBalance() const {
+	return minimumBalance;
+}
+// other methods
+void Withdraw() {
+	float temp;
+	cout << "Enter the withdrawal amount: $";
+	cin >> temp;
+	if ((acctBalance - temp) > minimumBalance) {
+		acctBalance -= temp;
+		cout << "Withdrew $" << fixed << setprecision(2) << temp << " from account id " << *ID << endl;
+	} else {
+		cout << "Error: failed to withdraw $" << fixed << setprecision(2) << temp << " from account id " << *ID << endl;
+	}
+}
+float GetAccountThreshold() const {
+	return minimumBalance;
+}
+// float minimumBalance;
