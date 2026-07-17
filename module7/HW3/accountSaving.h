d9bd9800b74f62ea524e4de3a1ac9c4774f9e73c
Wanderer2419 <wargamer34567@gmail.com>
2026-07-17 00:30:18 -0500

    Create accountSaving.h

---
+++ module7/HW3/accountSaving.h
@@ -0,0 +1,27 @@
+#ifndef ACCOUNTSAVING_H
+#define ACCOUNTSAVING_H
+
+#include "account.h"
+using namespace std;
+
+class AccountSaving : public Account {
+	public:
+		// constructors
+		AccountSaving();	// default constructor
+		~AccountSaving();	// destructor
+		AccountSaving (const AccountSaving& origObj);	// copy constructor
+		// operators
+		AccountSaving& operator=(const AccountSaving& rhs);
+
+		// minimumBalance functions
+		void SetMinBalance(float min);
+		float GetMinBalance() const;
+
+		// other methods
+		void Withdraw();
+		float GetAccountThreshold() const;
+	private:
+		float minimumBalance;
+};
+
+#endif
