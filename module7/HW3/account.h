8604b8986dae7e7b087cef209ad340760e056260
Wanderer2419 <wargamer34567@gmail.com>
2026-07-16 22:24:41 -0500

    Create account.h

---
+++ module7/HW3/account.h
@@ -0,0 +1,36 @@
+#ifndef ACCOUNT_H
+#define ACCOUNT_H
+
+#include <string>
+using namespace std;
+
+class Account {
+	private:
+		int* ID;
+		string customerName;
+		float acctBalance;
+	public:
+		// constructors
+		Account();	// default constructor
+		~Account();	// destructor
+		Account(const Account& origObj);
+		// operators
+		Account& operator=(const Account& rhs);
+		Account& operator+=(Account& rhs);
+
+		// mutators
+		void SetID(int id);
+		void SetCustName(string name);
+		void SetAcctBalance(float balance);
+
+		// accessors
+		int GetID() const;
+		string GetCustName() const;
+		float GetAcctBalance() const;
+
+		// member functions
+		void Deposit();
+		void Withdraw();
+};
+
+#endif
