89889a0a59f7b63c4ce02c5c3150d400d3fe36c2
Wanderer2419 <wargamer34567@gmail.com>
2026-07-17 00:19:44 -0500

    Create AccountChecking.cpp

---
+++ module7/HW3/AccountChecking.cpp
@@ -0,0 +1,58 @@
+#include "AccountChecking.h"
+#include <iostream>
+#include <iomanip>
+#include <string>
+using namespace std;
+
+// constructors
+AccountChecking::AccountChecking() { // default constructor
+	transactionFee = 0.00;
+}
+AccountChecking::~AccountChecking() { // destructor
+	delete ID;
+}
+AccountChecking::AccountChecking (const AccountChecking& origObj) { // copy constructor
+	ID = new int;
+	*ID = origObj.GetID();
+	customerName = origObj.GetCustName();
+	acctBalance = origObj.GetAcctBalance();
+	transactionFee = origObj.GetTransactionFee();
+}
+
+// operators
+AccountChecking::AccountChecking& operator=(const AccountChecking& rhs) {
+	if (this != &rhs) {
+		delete ID;
+		ID = new int;
+		*ID = rhs.GetID();
+		customerName = rhs.GetCustName();
+		acctBalance = rhs.GetAcctBalance();
+		transactionFee = origObj.GetTransactionFee();
+	}
+	return *this;
+}
+
+// other methods
+void AccountChecking::Deposit() {
+	float temp;
+	cout << "Enter the deposit amount: $";
+	cin >> temp;
+	acctBalance += (temp - transactionFee);
+	cout << "Deposited $" << fixed << setprecision(2) (temp - transactionFee) << " into account id" << *ID << endl;
+}
+void AccountChecking::Withdraw() {
+	float temp;
+	cout << "Enter the withdrawal amount: $";
+	cin >> temp;
+	if (temp <= acctBalance) {
+		acctBalance -= temp;
+		acctBalance -= transactionFee;
+		cout << "Withdrew $" << fixed << setprecision(2) << temp << " from account id " << *ID << endl;
+	} else {
+		cout << "Error: failed to withdraw $" << fixed << setprecision(2) << temp << " from account id " << *ID << endl;
+	}
+}
+float AccountChecking::GetAccountThreshold() const {
+	return transactionFee;
+}
+// float transactionFee;
