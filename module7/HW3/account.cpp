4dfbf61152e523afa8e5b8e0e636d317a2c89df2
Wanderer2419 <wargamer34567@gmail.com>
2026-07-18 00:46:26 -0500

    Update account.cpp

--- module7/HW3/account.cpp
+++ module7/HW3/account.cpp
@@ -1,115 +1,41 @@
-#include "account.h"
-#include <iostream>
-#include <iomanip>
+#include "accounts.h"
 using namespace std;
+//const int SIZE = 1000;
+//Account* first;
+//int countSize;
+//vector<Account *> acctsContainer;
 
 // constructors
-Account::Account() {	// default constructor
-	ID = new int;
-	*ID = -1;
-	customerName = "";
-	acctBalance = 0.0;
-	accountType = 0;
+Accounts::Accounts() {	//default constructor
+	countSize = 0;
 }
-Account::~Account() {	// destructor
-	delete ID;
-}
-Account::Account(const Account& origObj) {	// copy constructor
-	ID = new int;
-	*ID = origObj.GetID();
-	customerName = origObj.GetCustName();
-	acctBalance = origObj.GetAcctBalance();
-	accountType = 0;
+Accounts::~Accounts() {	// destructor
+	for (unsigned int i = 0; i < acctsContainer.size(); i++) {
+		delete acctsContainer.at(i);
+	}
+//	acctsContainer.clear();
+//	delete first;
+	countSize = 0;
 }
 
-// operators
-Account& Account::operator=(const Account& rhs) {
-	if (this != &rhs) {
-		delete ID;
-		ID = new int;
-		*ID = rhs.GetID();
-		customerName = rhs.GetCustName();
-		acctBalance = rhs.GetAcctBalance();
-	}
-	return *this;
+// accessor
+int Accounts::Size() const {
+	return countSize;
 }
-Account& Account::operator+=(Account& source) {
-	float temp;
-	cout << "Enter the transfer amount: $";
-	cin >> temp;
-	if (source.GetAcctType() == 1) {
-		if (temp < source.GetAcctBalance()) {
-			source.SetAcctBalance(source.GetAcctBalance() - temp);
-			this->acctBalance += temp;
-			cout << "Successfully transfered amount $" << fixed << setprecision(2) << temp << " from source acccoun id " << source.GetID() << " to destination account id " << this->GetID() << endl;
-		} else {
-			cout << "Error: Insufficient balance. Source account has a remaining balance of $" << source.GetAcctBalance() << endl;
-		}
-	} else if (source.GetAcctType() == 2) {
-		if (source.GetAccountThreshold() < (source.GetAcctBalance() - temp)) {
-			source.SetAcctBalance(source.GetAcctBalance() - temp);
-			this->acctBalance += temp;
-			cout << "Successfully transfered amount $" << fixed << setprecision(2) << temp << " from source acccoun id " << source.GetID() << " to destination account id " << this->GetID() << endl;
-		} else {
-			cout << "Error: Insufficient balance. Source account has a remaining balance of $" << source.GetAcctBalance() << endl;
+
+// methods
+void Accounts::addAccount(Account* input) {
+	acctsContainer.push_back(input);
+	countSize++;
+}
+int Accounts::searchID(const int id) {
+	for (int i = 0; i < countSize; i++) {
+		if (acctsContainer.at(i)->GetID() == id) {
+			return i;
 		}
 	}
-	return *this;
+	return -1;	// if index = -1, then the id was not found in the container
 }
-
-// mutators
-void Account::SetID(int id) {
-	delete ID;
-	ID = new int;
-	*ID = id;
-}
-void Account::SetCustName(string name) {
-	customerName = name;
-}
-void Account::SetAcctBalance(float balance) {
-	acctBalance = balance;
-}
-void Account::SetAcctType(int type) {
-	accountType = type;
-}
-
-// accessors
-int Account::GetID() const {
-	return *ID;
-}
-string Account::GetCustName() const {
-	return customerName;
-}
-float Account::GetAcctBalance() const {
-	return acctBalance;
-}
-int Account::GetAcctType() const {
-	return accountType;
-}
-
-// other member functions
-void Account::Deposit() {
-	float temp;
-	cout << "Enter the deposit amount: $";
-	cin >> temp;
-	acctBalance += temp;
-	cout << "Deposited $" << fixed << setprecision(2) << temp << " int account id " << *ID << endl;
-}
-void Account::Withdraw() {
-	float temp;
-	cout << "Enter the withdrawal amount: $";
-	cin >> temp;
-	if (temp <= acctBalance) {
-		acctBalance -= temp;
-		cout << "Withdrew $" << fixed << setprecision(2) << temp << " from account id " << *ID << endl;
-	} else {
-		cout << "Error: failed to withdraw $" << fixed << setprecision(2) << temp << " from account id " << *ID << endl;
-	}
-}
-
-virtual void SetTransactionFee(float fee) {
-	fee = 0;
-}
-virtual void SetMinBalance(float min) {
-	min = 0;
+Account& Accounts::at(const int i) const {
+	return (*acctsContainer.at(i));
 }
