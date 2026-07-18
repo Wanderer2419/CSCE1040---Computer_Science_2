1a092ab4e3a34dbd6121af478be575cc00ac36da
Wanderer2419 <wargamer34567@gmail.com>
2026-07-17 23:03:09 -0500

    Update accounts.cpp

--- module7/HW3/accounts.cpp
+++ module7/HW3/accounts.cpp
@@ -3,15 +3,13 @@ using namespace std;
 //const int SIZE = 1000;
 //Account* first;
 //int countSize;
-//Account* acctsArray;
+//vector<Account *> acctsContainer;
 
 // constructors
 Accounts::Accounts() {	//default constructor
-	acctsArray = new Account[SIZE];
 	countSize = 0;
 }
 Accounts::~Accounts() {	// destructor
-	delete[] acctsArray;
 //	delete first;
 	countSize = 0;
 }
@@ -23,22 +21,16 @@ int Accounts::Size() const {
 
 // methods
 void Accounts::addAccount(const Account& input) {
-	if (countSize < SIZE) {
-		acctsArray[countSize] = input;
-		if (countSize == 0) {	// if this is the first input into acctsArray, the point first to that first element
-			first = &acctsArray[0];
-		}
-		countSize++;
-	}
+	acctsContainer.push_back(input);
 }
 int Accounts::searchID(const int id) {
 	for (int i = 0; i < countSize; i++) {
-		if (acctsArray[i].GetID() == id) {
+		if (acctsContainer.at(i)->GetID() == id) {
 			return i;
 		}
 	}
 	return -1;	// if index = -1, then the id was not found in the container
 }
 Account& Accounts::at(const int i) const {
-	return (acctsArray[i]);
+	return (acctsContainer.at(i));
 }
